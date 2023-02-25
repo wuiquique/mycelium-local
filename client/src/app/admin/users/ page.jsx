4ad1c6a2b0b2da025558911cdddcd1bc2b473ba1
Diego <ddvallejoj@gmail.com>
2023-02-25 12:46:49 -0600

    user_detalle

--- client/src/app/admin/users/ page.jsx
+++ client/src/app/admin/users/ page.jsx
@@ -10,15 +10,32 @@ import {
   TextField,
   Typography,
   Table,
+  TableHead,
 } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 
 export default function adminusers() {
+
+  const [users, setUsers] = ([])
+  
+
   return (
     <div>
       <Grid2 container spacing={2}>
         <Grid2 lg={12}>
-          <Table></Table>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>First Name</TableCell>
+                <TableCell>Last Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+
+            </TableBody>
+          </Table>
         </Grid2>
       </Grid2>
     </div>
