c4d2a64feee121be4eb5250fe8bf28e6a4306e0f
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-03-19 09:11:00 -0600

    Try optimizing

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,22 @@
+// const withBundleAnalyzer = require("@next/bundle-analyzer")({
+//   enabled: process.env.ANALYZE === "true",
+// });
+// const path = require("node:path");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  modularizeImports: {
+    "@mui/icons-material": {
+      transform: "@mui/icons-material/{{member}}",
+    },
+    "@mui/material": {
+      transform: "@mui/material/{{member}}",
+    },
+  },
+};
 
-module.exports = nextConfig
+// module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
