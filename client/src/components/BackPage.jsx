dfbb42085351fa3ea78acc070266b6a2daa5b128
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-03-19 08:09:44 -0600

    Use native NextJS Router

--- client/src/components/BackPage.jsx
+++ client/src/components/BackPage.jsx
@@ -1,24 +1,14 @@
 "use client";
 
-import React, { useEffect, useState, useRouter } from "react";
-import {
-  Card,
-  CardContent,
-  CardMedia,
-  Button,
-  TextField,
-  Typography,
-  CardHeader,
-} from "@mui/material";
-import Grid2 from "@mui/material/Unstable_Grid2";
 import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
-import Link from "next/link";
+import { Button } from "@mui/material";
+import { useRouter } from "next/navigation";
 
 export default function BackPage() {
-  const router = useRouter;
+  const router = useRouter();
   return (
     <div className="justify-left block">
-      <Button size="large" component={Link} href="javascript:history.back()">
+      <Button size="large" onClick={() => router.back()}>
         <KeyboardBackspaceOutlinedIcon className="m-1" />
         Back
       </Button>
