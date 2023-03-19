9674928f2939a8e77585392c46af776af2c95450
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-03-19 09:20:14 -0600

    try optimizing pt. 2

--- client/src/theme.ts
+++ client/src/theme.ts
@@ -1,18 +1,18 @@
 import { createTheme } from "@mui/material/styles";
-import * as colors from "tailwindcss/colors";
 import type {} from "@mui/x-date-pickers/themeAugmentation";
+import { amber, red, rose } from "tailwindcss/colors";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
     primary: {
-      main: colors.rose[600],
+      main: rose[600],
     },
     secondary: {
-      main: colors.amber[600],
+      main: amber[600],
     },
     error: {
-      main: colors.red[600],
+      main: red[600],
     },
   },
 });
