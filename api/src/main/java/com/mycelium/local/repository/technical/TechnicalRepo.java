11482645fa02abf1fbd8b82ac78757f957170fcd
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-03-23 18:06:48 -0600

    Fix Technical Repo

--- api/src/main/java/com/mycelium/local/repository/technical/TechnicalRepo.java
+++ api/src/main/java/com/mycelium/local/repository/technical/TechnicalRepo.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.jdbc.annotation.JdbcRepository;
+import io.micronaut.data.model.query.builder.sql.Dialect;
 import io.micronaut.data.repository.CrudRepository;
 
 @Repository("default")
-@JdbcRepository
+@JdbcRepository(dialect = Dialect.ORACLE)
 public interface TechnicalRepo extends CrudRepository<Technical, Integer> {
     List<Technical> findByProductId(Integer productId);
 }
