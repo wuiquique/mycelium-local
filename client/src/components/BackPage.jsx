d2685cf39f890bf75295ca2a790e8d41ca918dc1
jrberganza <68700999+jrberganza@users.noreply.github.com>
2023-03-24 18:38:11 -0600

    Pictures & Technical in product creation + texts by ZazCaras

--- client/src/components/BackPage.jsx
+++ client/src/components/BackPage.jsx
@@ -1,16 +1,18 @@
 "use client";
 
+import { useTexts } from "@/hooks/textContext";
 import KeyboardBackspaceOutlinedIcon from "@mui/icons-material/KeyboardBackspaceOutlined";
 import { Button } from "@mui/material";
 import { useRouter } from "next/navigation";
 
 export default function BackPage() {
+  const texts = useTexts();
   const router = useRouter();
   return (
     <div className="justify-left block">
       <Button size="large" onClick={() => router.back()}>
         <KeyboardBackspaceOutlinedIcon className="m-1" />
-        Back
+        {texts.global.backbutton}
       </Button>
     </div>
   );
