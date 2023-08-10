488c39fc6dbc8b839f6dd8739f975cc9652c8957
wuiquique <luisenriquem15@gmail.com>
2023-08-10 11:25:54 -0600

    fix

--- api/src/main/java/com/mycelium/local/Application.java
+++ api/src/main/java/com/mycelium/local/Application.java
@@ -9,19 +9,6 @@ import io.swagger.v3.oas.annotations.servers.Server;
 public class Application {
 
     public static void main(String[] args) {
-
-        // NOSONAR
-        int x = 10;
-        if (x > 5) {
-            System.out.println("x es mayor que 5");
-        } else {
-            for (int i = 0; i < 10; i++) {
-                System.out.println(i);
-            }
-}
-
-}
-
         Micronaut.run(Application.class, args);
     }
 }
