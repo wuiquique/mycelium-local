b2546608a3dbe4c85367777352e6f0c889f646c4
wuiquique <luisenriquem15@gmail.com>
2023-08-10 19:10:46 -0600

    cambio de hora

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -44,7 +44,7 @@ export function Page() {
         </div>
         <Typography variant="h4" mt={2}>
           {/* {texts.global.shopname} */}
-          Regina Coeli 18:20pm UAT
+          Regina Coeli 19:10pm UAT
         </Typography>
         <Typography variant="h6" m={1}>
           {texts.homepage.title}
