e7ab96bcb37bd6b9c2f6999d3a736c91232bbdda
wuiquique <luisenriquem15@gmail.com>
2023-09-08 17:57:21 -0600

    2023-MagicDev duda 2.0

--- api/src/main/java/com/mycelium/local/ApiController.java
+++ api/src/main/java/com/mycelium/local/ApiController.java
@@ -12,7 +12,7 @@ public class ApiController {
     @Get(uri = "/", produces = "text/plain")
     public String index() {
         while (true) {
-            const a = 1;
+            //ola
         }
         return "Example Response";
     }
