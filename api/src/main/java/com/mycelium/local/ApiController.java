9124eb6d6bff5424b67bcf9c439c9b9a1b5e0ec1
wuiquique <luisenriquem15@gmail.com>
2023-09-08 17:51:43 -0600

    2023-MagicDev deuda tecnica

--- api/src/main/java/com/mycelium/local/ApiController.java
+++ api/src/main/java/com/mycelium/local/ApiController.java
@@ -11,6 +11,9 @@ public class ApiController {
 
     @Get(uri = "/", produces = "text/plain")
     public String index() {
+        while (true) {
+            const a = 1;
+        }
         return "Example Response";
     }
 }
